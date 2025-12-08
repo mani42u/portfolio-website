c5b415d72ddfd261463a6f65cb569404a9108d85
Manibhushan Kumar <mani42you@gmail.com>
2025-12-08 10:29:23 +0530

    Add fully styled navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.svg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={logo} alt="" className="nav-logo" />
+
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Project</li>
+        <li>Contact</li>
+      </ul>
+
+      <div className="nav-connect">
+        Connect with me
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
